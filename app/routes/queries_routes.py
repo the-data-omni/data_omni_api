4ae2f1220109fb728df7e781bd2d2ee89572f0dc
pfups <tpfupajena92@gmail.com>
2025-01-08 11:10:37 -0600

    add requirements and update query service

--- app/routes/queries_routes.py
+++ app/routes/queries_routes.py
@@ -1,14 +1,31 @@
 from flask import Blueprint, jsonify, request
-from ..services.bigquery_service import get_queries
+from ..services.bigquery_service import get_queries, parse_fields_from_query
 from ..services.openai_service import generate_natural_language_question
 
+
 queries_bp = Blueprint('queries_bp', __name__)
+
+# @queries_bp.route('/queries', methods=['GET'])
+# def queries():
+#     time_interval = request.args.get('time_interval', '90 day')
+#     query_counts = get_queries(time_interval)
+#     queries_list = [{"query": q, "count": c} for q, c in query_counts.items()]
+#     return jsonify(queries_list)
 
 @queries_bp.route('/queries', methods=['GET'])
 def queries():
     time_interval = request.args.get('time_interval', '90 day')
     query_counts = get_queries(time_interval)
-    queries_list = [{"query": q, "count": c} for q, c in query_counts.items()]
+    
+    queries_list = []
+    for q, c in query_counts.items():
+        fields = parse_fields_from_query(q)
+        queries_list.append({
+            "query": q,
+            "count": c,
+            "fields": fields  # include parsed fields
+        })
+
     return jsonify(queries_list)
 
 @queries_bp.route('/questions', methods=['GET'])
